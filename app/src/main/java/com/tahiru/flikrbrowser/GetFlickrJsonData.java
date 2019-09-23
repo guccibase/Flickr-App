34f228c7600bf5749f0765e1c4d733d77b57cd4c
guccibase <tahirnasri96@gmail.com>
2019-09-22 23:27:34 -0400

    first commit

---
+++ app/src/main/java/com/tahiru/flikrbrowser/GetFlickrJsonData.java
@@ -0,0 +1,103 @@
+package com.tahiru.flikrbrowser;
+
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class GetFlickrJsonData implements GetRawData.OnDownloadComplete {
+
+    private static final String TAG = "GetFlickrJsonData";
+    private final OnDataAvailable mCallBack;
+    private List<Photo> mPhotoList = null;
+    private String mBaseURL;
+    private String mLanguage;
+    private boolean mMatchAll;
+
+    public GetFlickrJsonData(OnDataAvailable mCallBack, String mBaseURL, String mLanguage, boolean mMatchAll) {
+        Log.d(TAG, "GetFlickrJsonData: called");
+        this.mBaseURL = mBaseURL;
+        this.mLanguage = mLanguage;
+        this.mMatchAll = mMatchAll;
+        this.mCallBack = mCallBack;
+    }
+
+    void executeOnSameThread(String searchCriteria) {
+        Log.d(TAG, "executeOnSameThread: Starts");
+        String destinationUri = createUri(searchCriteria, mLanguage, mMatchAll);
+
+        GetRawData getRawData = new GetRawData(this);
+        getRawData.execute(destinationUri);
+        Log.d(TAG, "executeOnSameThread: ends");
+
+    }
+
+    private String createUri(String searchCriteria, String mLanguage, boolean mMatchAll) {
+        Log.d(TAG, "createUri: Starts");
+
+        return Uri.parse(mBaseURL).buildUpon()
+
+                .appendQueryParameter("tags", searchCriteria)
+                .appendQueryParameter("tagmode", mMatchAll ? "ALL" : "ANY")
+                .appendQueryParameter("lang", mLanguage)
+                .appendQueryParameter("format", "json")
+                .appendQueryParameter("nojsoncallback", "1")
+                .build().toString();
+
+    }
+
+    @Override
+    public void onDownloadComplete(String data, DownloadStatus status) {
+        Log.d(TAG, "onDownloadComplete: Status = " + status);
+
+        if (status == DownloadStatus.OK) {
+            mPhotoList = new ArrayList<>();
+
+            try {
+                JSONObject jsonData = new JSONObject(data);
+                JSONArray itemsArray = jsonData.getJSONArray("items");
+
+                for (int i = 1; i < itemsArray.length(); i++) {
+                    JSONObject jsonPhoto = itemsArray.getJSONObject(i);
+                    String title = jsonPhoto.getString("title");
+                    String author = jsonPhoto.getString("author");
+                    String authoId = jsonPhoto.getString("author_id");
+                    String tags = jsonPhoto.getString("tags");
+
+                    JSONObject jsonMedia = jsonPhoto.getJSONObject("media");
+                    String photoUrl = jsonMedia.getString("m");
+
+                    String link = photoUrl.replaceFirst("_m.", "_b");
+
+                    Photo photoObject = new Photo(title, author, authoId, link, tags, photoUrl);
+                    mPhotoList.add(photoObject);
+
+                    Log.d(TAG, "onDownloadComplete: " + photoObject.toString());
+
+                }
+
+            } catch (JSONException json) {
+                json.printStackTrace();
+                Log.e(TAG, "onDownloadComplete: Error processing json data " + json.getMessage());
+                status = DownloadStatus.FAILED_OR_EMPTY;
+            }
+        }
+
+        if (mCallBack != null) {
+            //now inform the caller that processing is done - possibly returning null if there
+            // was an error
+            mCallBack.onDataAvailable(mPhotoList, status);
+        }
+
+        Log.d(TAG, "onDownloadComplete: ends");
+    }
+
+    interface OnDataAvailable {
+        void onDataAvailable(List<Photo> data, DownloadStatus status);
+    }
+}
---
+++ app/src/main/java/com/tahiru/flikrbrowser/MainActivity.java
@@ -0,0 +1,73 @@
+package com.tahiru.flikrbrowser;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements GetFlickrJsonData.OnDataAvailable {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: Starts");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+//
+//        GetRawData getRawData = new GetRawData(this);
+//        getRawData.execute("https://www.flickr.com/services/feeds/photos_public.gne?tags=android,nougat,sdk&tagmode=any&format=json&nojsoncallback=1");
+
+        Log.d(TAG, "onCreate: Ends");
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume: Starts");
+        super.onResume();
+        GetFlickrJsonData getFlickrJsonData = new GetFlickrJsonData(this, "https://www.flickr.com/services/feeds/photos_public.gne", "en-us", true);
+        getFlickrJsonData.executeOnSameThread("android, nougat");
+        Log.d(TAG, "onResume: ends");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.d(TAG, "onCreateOptionsMenu() returned: " + true);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        Log.d(TAG, "onOptionsItemSelected() returned: Returned");
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDataAvailable(List<Photo> data, DownloadStatus status) {
+        if (status == DownloadStatus.OK) {
+            Log.d(TAG, "onDataAvailable: data is " + status);
+        } else {
+
+            // download or processing failed
+            Log.d(TAG, "onDataAvailable: failed with status " + status);
+        }
+    }
+}
